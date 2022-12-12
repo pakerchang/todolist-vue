d07595209a07bfb4064906e801020ecce33e4743
pakerchang <pakerchang@gmail.com>
2022-12-12 15:12:37 +0800

    feat(addTask): #4 Add task event.

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -2,14 +2,19 @@
 import { useTaskList } from "@/store/taskList";
 import { Combobox, ComboboxInput } from "@headlessui/vue";
 import { PlusIcon } from '@heroicons/vue/24/outline';
-import { ref } from 'vue'
+import { ref } from 'vue';
+
+const taskQuery = ref('');
+const taskStore = useTaskList();
+
+const onSubmit = () => taskStore.addTask(taskQuery.value);
 
 </script>
 <template>
   <Combobox>
     <div class="w-full flex">
-      <ComboboxInput />
-      <button>
+      <ComboboxInput @change="taskQuery = $event.target.value" @keyup.enter="onSubmit" />
+      <button @click="onSubmit">
         <PlusIcon class="w-6 h-6" />
       </button>
     </div>
